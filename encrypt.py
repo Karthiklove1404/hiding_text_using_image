d7662e5e596fd67038039cecf42508e6e42f886d
Karthiklove1404 <128292632+Karthiklove1404@users.noreply.github.com>
2024-02-26 22:34:03 +0530

    encrypt.py
    Signed-off-by: Karthiklove1404 <128292632+Karthiklove1404@users.noreply.github.com>

--- encrypt.py
+++ encrypt.py
@@ -2,4 +2,6 @@ from stegano import lsb
 inp_img =input("enter image path or your image :")
 message = input("enter your message to hide : ")
 secreat_msg = lsb.hide(inp_img, message)
-secreat_msg.save("encrypt.png")
+print("file save in .png")
+save_img = input("enter name : ")
+secreat_msg.save(save_img)
